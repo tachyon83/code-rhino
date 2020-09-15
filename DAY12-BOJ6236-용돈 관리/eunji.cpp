e4e2480a4901ace8589b423835a45db17a4cbf2c
Eunji <45475492+dmswl6310@users.noreply.github.com>
2020-09-15 14:45:49 +0900

    실패!ㅜㅜ
    백준 테케는 맞게 나오는데 제출하면 틀렸다고 뜹니다.. 예외가 뭘까요???

---
+++ DAY12-BOJ6236-용돈 관리/eunji.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+int countday(int out, int use[], int N);
+
+int main() {
+	int N, M;
+	int start=0;
+	int end = 1000000000;
+	int mid;
+	cin >> N >> M;
+	int use[100001] = { 0, };
+
+	for (int i = 0; i < N; i++) {
+		cin >> use[i];
+		start = start < use[i] ? use[i] : start;
+	}
+
+	while (start<=end) {
+		mid = (start + end) / 2;
+		int count = countday(mid, use, N);
+		if (count == M) {
+			break;
+		}
+		else if (count > M){
+		start = mid + 1;
+
+		}
+		else {
+			end = mid - 1;
+		}
+	}
+	cout << mid;
+}
+
+int countday(int out, int use[],int N) {
+	int have = 0;
+	int count = 0;;
+	for (int i = 0; i < N; i++) {
+		if (have < use[i]) {
+			have = out;
+			count++;
+		}
+			have -= use[i];
+	}
+	return count;
+}
