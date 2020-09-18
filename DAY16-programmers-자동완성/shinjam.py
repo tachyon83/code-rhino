5910355370c4b704fd20bc58b97f36f6348bd4fa
Jae Min Shin <nevvjann@gmail.com>
2020-09-18 22:31:17 +0900

    :x: : 프로그래머스-자동완성

--- DAY16-programmers-자동완성/shinjam.py
+++ DAY16-programmers-자동완성/shinjam.py
@@ -3,18 +3,14 @@ from collections import defaultdict
 def solution(words):
     hash_dict = defaultdict(int)
     for word in words:
-        s = ''
-        for idx, val in enumerate(word):
-            s += val
-            hash_dict[s] += 1
+        for i in range(len(word)):            
+            hash_dict[word[:i + 1]] += 1
     
     
     cnt = 0
     for word in words:
-        s = ''
-        for idx, val in enumerate(word):
-            s += val
+        for i in range(len(word)):  
             cnt += 1
-            if hash_dict[s] == 1:
+            if hash_dict[word[:i + 1]] == 1:
                 break
     return cnt
