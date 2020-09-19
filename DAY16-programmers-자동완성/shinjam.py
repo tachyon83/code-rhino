b3337ac8a96e0867a9d7d960673aecf2ff464f2b
Jae Min Shin <nevvjann@gmail.com>
2020-09-20 00:57:20 +0900

    :heavy_check_mark: : (수정)프로그래머스-자동완성

--- DAY16-programmers-자동완성/shinjam.py
+++ DAY16-programmers-자동완성/shinjam.py
@@ -1,27 +1,30 @@
+import sys
 from collections import defaultdict
-import sys 
-sys.setrecursionlimit(10**8)
+
+sys.setrecursionlimit(10 ** 8)
+
 
 class Node:
-    def __init__(self, cnt = 0):
+    def __init__(self, cnt=0):
         self.cnt = cnt
         self.children = defaultdict(Node)
-        
-def add(root, word, idx = 0):
-    if idx == len(word): 
+
+
+def add(root, word, idx=0):
+    if idx == len(word):
         return
     root.children[word[idx]].cnt += 1
     add(root.children[word[idx]], word, idx + 1)
 
-def check(root, word, idx = 0):
-    if idx == len(word): 
+
+def check(root, word, idx=0):
+    if idx == len(word):
         return idx
-    if root.children[word[idx]].cnt == 1 :
+    if root.children[word[idx]].cnt == 1:
         return idx + 1
     return check(root.children[word[idx]], word, idx + 1)
 
 
-        
 def solution(words):
     root = Node()
     ret = 0
@@ -29,5 +32,5 @@ def solution(words):
         add(root, word)
     for word in words:
         ret += check(root, word)
-    
+
     return ret
