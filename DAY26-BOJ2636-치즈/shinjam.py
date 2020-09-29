9996fd0b101266ef3584c122ca572e27e74736df
Jae Min Shin <nevvjann@gmail.com>
2020-09-29 13:24:53 +0900

    :heavy_check_mark: : (updated)boj_2636-치즈

--- DAY26-BOJ2636-치즈/shinjam.py
+++ DAY26-BOJ2636-치즈/shinjam.py
@@ -1,17 +1,9 @@
 from collections import deque
 
-def get_cheeses(desk):
-    ret = set()
-    for idx in range(col * row):
-        c, r = divmod(idx, row)
-        if desk[c][r] == 1:
-            ret.add((r, c))
-    return ret
 
-
-def will_melts(cheeses, visited):
+def will_melts():
     queue = deque([(0, 0)])
-    melts = set()
+    visited, melts = set(), set()
     while queue:
         x, y = queue.popleft()
         if desk[y][x] == 1:
@@ -19,24 +11,24 @@ def will_melts(cheeses, visited):
             continue
         for v in range(4):
             nx, ny = x + dx[v], y + dy[v]
-            if all([0<=nx<row, 0<=ny<col]) and (nx, ny) not in visited:
+            if (0<=nx<row and 0<=ny<col) and (nx, ny) not in visited:
                 visited.add((nx, ny))
                 queue.append((nx, ny))
     for x, y in melts:
         desk[y][x] = 0
-    return melts
+    return len(melts)
 
 
 col, row = map(int, input().split())
 desk = [list(map(int, input().split()))for _ in range(col)]
-cheeses = get_cheeses(desk)
+cheeses = sum(sum(desk, []))
 time, left_cnt = 0, 0
 
 dx, dy = (1, 0, -1, 0), (0, 1, 0, -1)
 
 while cheeses:
-    left_cnt = len(cheeses)
-    cheeses -= will_melts(cheeses, set())
+    left_cnt = cheeses
+    cheeses -= will_melts()
     time += 1
     
 print(time)
