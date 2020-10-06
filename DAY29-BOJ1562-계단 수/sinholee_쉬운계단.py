b5e0755990ab886a1eb56ead1547387cca523ee0
이신호 <dltlsgh5@naver.com>
2020-10-06 17:49:14 +0900

    이신호 2020-10-06 계단수 못품... 쉬운계단수 먼저 품

---
+++ DAY29-BOJ1562-계단 수/sinholee_쉬운계단.py
@@ -0,0 +1,20 @@
+'''
+일단 쉬운계단수부터 풀어봤습니다. 어려운,...계단수도 빨리 풀어볼게여ㅠㅠ
+'''
+
+n = int(input())
+
+dp = [[0] * 10 for _ in range(n+1)]
+for i in range(1, 10):
+    dp[1][i] = 1
+for i in range(2, n+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][1]
+        elif j == 9:
+            dp[i][j] = dp[i-1][8]
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+
+print(sum(dp[n])%1000000000)
+
