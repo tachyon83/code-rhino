e57f523ae7229335c5178acdf4e49521291ffc73
Jae Min Shin <nevvjann@gmail.com>
2020-10-06 13:22:09 +0900

    :heavy_check_mark: boj1562-계단수

--- DAY29-BOJ1562-계단 수/shinjam.py
+++ DAY29-BOJ1562-계단 수/shinjam.py
@@ -11,15 +11,13 @@ for n in range(N-1):
     for i in range(10):
         for key, val in DP[i].items():
             if i < 9:
-                tmp[i + 1][key | 1<<i] = tmp[i + 1][key | 1<<i] + val
+                tmp[i + 1][key | 1<<(i+1)] = tmp[i + 1][key | 1<<(i+1)] + val
             if 0 < i:
-                tmp[i - 1][key | 1<<i] = tmp[i - 1][key | 1<<i] + val
+                tmp[i - 1][key | 1<<(i-1)] = tmp[i - 1][key | 1<<(i-1)] + val
 
     DP.update(tmp)
     
 ret = 0
 for i in range(10):
     ret += DP[i][1023]
-print(ret)
-
-# 왜 안되는걸까요 ㅜㅠ
+print(ret%10**9)
