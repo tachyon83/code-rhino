3051eb49a9a2cab7e7a418d5970ae18abea53a6f
younghoonkwon <younghoon.kwon91@gmail.com>
2020-10-07 16:01:28 +0900

    Update and rename (incomplete)younghoon.py to younghoon.py

--- DAY26-BOJ2636-치즈/younghoon.py
+++ DAY26-BOJ2636-치즈/younghoon.py
@@ -1,29 +1,31 @@
-#나중에 시간날때 BFS로 다시 구현 하겠습니다
-
-
 import copy
-h, w = map(int, input().split())
-grid = list(list(map(int, input().split())) for _ in range(h))
+from collections import deque
+import sys
+#sys.stdin = open("input.txt", "r")
+h, w = map(int, sys.stdin.readline().split())
+grid = list(list(map(int, sys.stdin.readline().split())) for _ in range(h))
 # 동서남북
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 gridcopy = copy.deepcopy(grid)
 visited = [[0 for i in range(w)] for j in range(h)]
 
-def dfs(x,y):
+def bfs(x,y):
     visited[x][y] = 1
-    if x == h - 1 and y == w -1:
-        return
-    for i in range(4):
-        nx = x+dx[i]
-        ny = y+dy[i]
-        if nx < 0 or nx >= h or ny < 0 or ny >= w or visited[nx][ny] == 1:
-            continue
-        else:
-            if grid[nx][ny] == 1:
-                gridcopy[nx][ny] = 'c'
+    q = deque([(x,y)])
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if nx < 0 or nx >= h or ny < 0 or ny >= w or visited[nx][ny] == 1:
                 continue
-            dfs(nx,ny)
+            else:
+                if grid[nx][ny] == 1:
+                    gridcopy[nx][ny] = 'c'
+                    continue
+                q.append([nx, ny])
+                visited[nx][ny] = 1
 
 def all_zero():
     for x in range(h):
@@ -65,11 +67,12 @@ melting_time = 0
 while all_zero() == False:
     prev = copy.deepcopy(grid)
     reset_visited()
-    dfs(0,0)
+    bfs(0,0)
+    # print_grid(gridcopy)
     melting_time += 1
     c_to_zero()
     grid = copy.deepcopy(gridcopy)
+    # break
 
 print(melting_time)
 print(cheese_count(prev))
-
