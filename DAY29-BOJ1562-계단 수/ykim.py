f498397e31854cb915ad20d1ecd6ec067165071d
kimys0279 <68838464+kimys0279@users.noreply.github.com>
2020-10-07 15:01:24 +0900

    Update ykim.py

--- DAY29-BOJ1562-계단 수/ykim.py
+++ DAY29-BOJ1562-계단 수/ykim.py
@@ -1,20 +1,27 @@
 '''
-<쉬운 계단 수>
+개념이해에 도움이 된 블로그
+https://m.blog.naver.com/PostView.nhn?blogId=occidere&logNo=221130521559&proxyReferer=https:%2F%2Fwww.google.com%2F
+'''
 
 
 n = int(input())
-dp = [[0 for i in range(10)] for j in range(101)]
+ 
+re = 0
+dp = [[0 for _ in range(1024)] for _ in range(10)]
+ 
 for i in range(1, 10):
-    dp[1][i] = 1
-for i in range(2, n + 1):
-    for j in range(10):
-        if j == 0:
-            dp[i][j] = dp[i - 1][1]
-        elif j == 9:
-            dp[i][j] = dp[i - 1][8]
-        else:
-            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
-print(sum(dp[n]) % 1000000000)
-'''
+    dp[i][1<<i] = 1
+ 
+for i in range(1, n):
+    dp_next = [[0 for _ in range(1024)] for _ in range(10)]
+    for e in range(10):
+        for bm in range(1024):
+            if e < 9:
+                dp_next[e][bm | (1<<e)] = (dp_next[e][bm | (1<<e)] + dp[e+1][bm]) % 1000000000
+            if e > 0:
+                dp_next[e][bm | (1<<e)] = (dp_next[e][bm | (1<<e)] + dp[e-1][bm]) % 1000000000
+    dp = dp_next
+ 
+print(sum([dp[i][1023] for i in range(10)]) % 1000000000)
 
 
