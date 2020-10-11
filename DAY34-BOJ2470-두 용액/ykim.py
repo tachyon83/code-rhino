f0b661ab10eaaed5b5b8324fc733e96c80be8054
kimys0279 <68838464+kimys0279@users.noreply.github.com>
2020-10-11 17:56:16 +0900

    Create ykim.py

---
+++ DAY34-BOJ2470-두 용액/ykim.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+liquid = [int(x) for x in input().split()]
+liquid.sort()
+left = 0
+right = N - 1
+answer = liquid[left] + liquid[right]
+al = left
+ar = right
+while left < right:
+    tmp = liquid[left] + liquid[right]
+    if abs(tmp) < abs(answer):
+        answer = tmp
+        al = left
+        ar = right
+        if answer == 0:
+            break
+    if tmp < 0:
+        left += 1
+    else:
+        right -= 1
+print(liquid[al], liquid[ar])
