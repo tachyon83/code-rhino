31353b94cea447af5c49c4a45aff22cf1171ca68
younghoonkwon <younghoon.kwon91@gmail.com>
2020-10-12 14:54:50 +0900

    Create (incomplete)younghoon-combinations.py

---
+++ DAY35-BOJ1759-암호만들기/(incomplete)younghoon-combinations.py
@@ -0,0 +1,22 @@
+'''
+이것도 틀렸다고 나와요
+오늘 왜이러죠....
+'''
+
+import sys
+from itertools import combinations
+#sys.stdin = open("input.txt", "r")
+L, C = map(int, sys.stdin.readline().split(' '))
+chars = list(sys.stdin.readline().split(' '))
+chars.sort()
+
+all_outputs = list(combinations(chars,L))
+vowls = ['a', 'e', 'i', 'o', 'u']
+
+for case in all_outputs:
+    v_count = 0
+    for char in case:
+        if char in vowls:
+            v_count += 1
+    if v_count>= 1 and len(case) - v_count >= 2:
+       print(''.join(case))
