88e7caaf044ba15c1f79d7b1b01dbbd33ef76415
Eunji <45475492+dmswl6310@users.noreply.github.com>
2020-10-19 20:21:46 +0900

    Update eunji.cpp

--- DAY41-BOJ2056-작업/eunji.cpp
+++ DAY41-BOJ2056-작업/eunji.cpp
@@ -3,13 +3,14 @@
 #include <queue>
 using namespace std;
 //최소시간을 찾아야합니다. 모든 작업을 해야하므로, 적어도 시간이 제일 많이 걸리는 길보다 더 커야합니다. 그래서 우선순위를 쓰게되었습니다. 
+
 int main() {
 	int N;
 	cin >> N;
 	vector<int> indegree(N+1, 0);
 	vector<int> cost(N+1, 0);
 	vector <vector<int>> connect(N+1,vector<int>());
-	priority_queue <int> q;
+	priority_queue <pair<int,int>> q;
 	for (int i = 1; i <= N; i++) {
 		cin >> cost[i];
 		cin >> indegree[i];
@@ -20,15 +21,23 @@ int main() {
 		}
 	}
 
-	q.push(1);
+	for (int i = 1; i <= N; i++) {
+		if (indegree[i] == 0) {
+			q.push({-cost[i],i });
+		}
+	}
+	int totalcost = 0;
 	while (!q.empty()) {
-		int tmp = q.front();
-		cout << tmp << endl;
+		int next = q.top().second;
+		int nextcost = -(q.top().first);
+		//cout << tmp << endl;
+		if (totalcost < nextcost) totalcost = nextcost;
 		q.pop();
-		for (int i = 0; i < connect[tmp].size(); i++) {
-			if (--indegree[connect[tmp][i]] == 0) {
-				q.push(connect[tmp][i]);
+		for (int i = 0; i < connect[next].size(); i++) {
+			if (--indegree[connect[next][i]] == 0) {
+				q.push({ -(cost[connect[next][i]]+nextcost),connect[next][i] });
 			}
 		}
 	}
+	cout << totalcost;
 }
