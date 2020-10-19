1ef1249eb5b8c275f2b24a90992d08e27702ceca
younghoonkwon <younghoon.kwon91@gmail.com>
2020-10-19 16:12:14 +0900

    Create younghoon.py

---
+++ DAY42-BOJ17298-오큰수/younghoon.py
@@ -0,0 +1,13 @@
+import sys
+#sys.stdin = open("input.txt", "r")
+stack = []
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+
+result = [-1 for _ in range(N)]
+
+for i in range(N):
+    while stack and arr[stack[-1]]<arr[i]:
+        result[stack.pop()] = arr[i]
+    stack.append(i)
+print(result)
