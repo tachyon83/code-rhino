b6a9089ce7fc8902fca400b9c1d74c5e909aa523
younlll <62369538+younlll@users.noreply.github.com>
2020-10-21 05:30:58 +0900

    Create yunalee.cpp

---
+++ DAY41-BOJ2056-작업/yunalee.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+using namespace std;
+const int MAX = 100000;
+
+int main() {
+	int idx[MAX] = { 0 }, n, a[MAX], d[MAX] = { 0 };
+	queue<int> Queue;
+	vector<int> arr[MAX];
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int k, x;
+		cin >> k >> x;
+		a[i] = k;
+		for (int j = 0, v; j < x; j++) {
+			cin >> v;
+			arr[v].push_back(i);
+			idx[i]++;
+		}
+	}
+
+	for (int i = 1; i <= n; i++){
+		if (idx[i] == 0) {
+			Queue.push(i);
+			d[i] = a[i];
+		}
+	}
+
+
+	while (!Queue.empty()) {
+		int cur = Queue.front();
+		Queue.pop();
+
+		for (int next : arr[cur]) {
+			d[next] = max(d[next], d[cur] + a[next]);
+			idx[next]--;
+			if (idx[next] == 0) {
+				Queue.push(next);
+
+			}
+		}
+	}
+
+	int result = 0;
+	for (int i = 1; i <= n; i++) {
+		result = max(result, d[i]);
+	}
+
+	cout << result << '\n';
+
+}
