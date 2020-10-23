aecc449eb78173f1e1b27e583f65c0fc1f6a0712
Eunji <45475492+dmswl6310@users.noreply.github.com>
2020-10-23 14:19:06 +0900

    성공

--- DAY46-BOJ2533-사회망 서비스(SNS)/eunji.cpp
+++ DAY46-BOJ2533-사회망 서비스(SNS)/eunji.cpp
@@ -1,3 +1,5 @@
+//처음에 메모리초과 났는데 2차원벡터에서 배여롤 바꾸니 해결됏어용 ㅎㅎ
+
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -5,58 +7,63 @@
 
 using namespace std;
 
-void join(int n1, int n2, vector<vector<int>> &tree);
-void bfs(int num, vector<vector<int>> tree, vector<vector<int>> &dp, vector<bool> &visit);
+void join(int n1, int n2);
+void bfs(int num, int parent);
 
+vector<int> tree[1000001];
+int dp[1000001][2] = { 0, };
 int main() {
 	int N;
 	cin >> N;
-	vector<vector<int>> tree(N + 1, vector<int>());
-	vector<vector<int>> dp(N + 1, vector<int>(2, -1));
-	vector<bool> visit(N + 1, false);
+
+	//vector<vector<int>> tree(N + 1, vector<int>());
+	//vector<vector<int>> dp(N + 1, vector<int>(2, -1));
+	//	vector<bool> visit(N + 1, false);
 	for (int i = 0; i < N - 1; i++) {
 		int n1, n2;
 		cin >> n1 >> n2;
-		join(n1, n2, tree);
+		join(n1, n2);
 
 	}
-	int total = 0;
-	for (int i = 1; i < visit.size();i++) {
-		if (visit[i] == false) {
-			bfs(i, tree, dp, visit);
-			total += (dp[1][0] < dp[1][1] ? dp[1][0] : dp[1][1]);
-		}
-	}
+
+	bfs(1, 0);
+	int total = (dp[1][0] < dp[1][1] ? dp[1][0] : dp[1][1]);
+
 	cout << total;
 
 }
 
-void join(int n1, int n2, vector<vector<int>> &tree) {
+void join(int n1, int n2) {
 	tree[n1].push_back(n2);
 	tree[n2].push_back(n1);
 
 }
 
-void bfs(int num, vector<vector<int>> tree, vector<vector<int>> &dp, vector<bool> &visit) {
-	visit[num] = true;
-	if (tree[num].size() == 1) {
+void bfs(int num, int parent) {
+	//visit[num] = true;
+
+	if (num != 1 && (tree[num].size() == 1)) {
 		dp[num][0] = 0;
 		dp[num][1] = 1;
 		return;
 	}
+
 	int use = 0;
 	int unuse = 0;
 	for (int i = 0; i < tree[num].size(); i++) {
-		if (visit[tree[num][i]] == true) {
+		if (parent == tree[num][i]) {
 			continue;
 		}
-		if (dp[tree[num][i]][0] == -1) {
-			bfs(tree[num][i], tree, dp, visit);
+		if (dp[tree[num][i]][0] == 0) {
+			bfs(tree[num][i], num);
 		}
 		int childuse = dp[tree[num][i]][1];
 		int childunuse = dp[tree[num][i]][0];
-		unuse += dp[tree[num][i]][1];
+
+		unuse += childuse;
 		use += (childuse > childunuse ? childunuse : childuse);
+
+
 	}
 	dp[num][0] = unuse;
 	dp[num][1] = use + 1;
