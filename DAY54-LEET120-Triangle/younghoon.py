4f0af577173858e171a54a68466bbb7c04990233
younghoonkwon <younghoon.kwon91@gmail.com>
2020-10-31 17:51:40 +0900

    Create younghoon.py

---
+++ DAY54-LEET120-Triangle/younghoon.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        if not triangle:
+            return
+        for i in range(1, len(triangle)):
+            for j in range(len(triangle[i])):
+                if j == 0:
+                    triangle[i][j] += triangle[i-1][j]
+                elif j == len(triangle[i])-1:
+                    triangle[i][j] += triangle[i-1][j-1]
+                else:
+                    triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j])
+        return min(triangle[len(triangle)-1])
