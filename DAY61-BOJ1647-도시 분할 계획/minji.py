7ad1ed7526374db5195ed600e62dff69c4892d63
Minji KANG <35449625+MinjiKang95@users.noreply.github.com>
2020-11-07 16:09:43 +0900

    Create minji.py

---
+++ DAY61-BOJ1647-도시 분할 계획/minji.py
@@ -0,0 +1,39 @@
+import sys
+
+def find(parent, x): #cycle check
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+
+def union(parent, a, b): #set
+    a = find(parent, a)
+    b = find(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+if __name__ == "__main__":
+    n, m = map(int, sys.stdin.readline().split()) #v, e
+    parent = [0] * (n+1)
+    edges = []
+    res = 0
+
+    for i in range(1, n+1):
+        parent[i] = i
+
+    for _ in range(m):
+        a, b, cost = map(int, sys.stdin.readline().split())
+        edges.append((cost, a, b))
+    edges.sort()
+
+    sel = []
+    for cost, a, b in edges:
+        if find(parent, a) != find(parent, b):
+            union(parent, a, b)
+            res += cost
+            sel.append(cost)
+
+    res -= sel.pop()
+    print(res)
