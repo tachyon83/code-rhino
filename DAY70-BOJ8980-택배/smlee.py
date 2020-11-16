d5d93fcec0a4048aa8861810322a4fb51f6d9546
seungmin <48234814+happylsm@users.noreply.github.com>
2020-11-16 21:05:51 +0900

    성공성공

--- DAY70-BOJ8980-택배/smlee.py
+++ DAY70-BOJ8980-택배/smlee.py
@@ -8,14 +8,15 @@ box_list.sort(key=lambda box_list: (box_list[1], box_list[0]))
 
 for i in range(m) :
     deli = box_list[i][2]
-    for x in range(box_list[i][0]-1,box_list[i][1]-1) :
-        if town[x] >= box_list[i][2] :
-            town[x] -= deli
-        elif town[x] == 0 :
-            deli = 0
+    if 0 not in town[box_list[i][0]-1:box_list[i][1]]:
+        if min(town[box_list[i][0]-1:box_list[i][1]]) >= box_list[i][2] :
+            pass
         else :
-            deli = town[x]
-            town[x] = 0
+            deli = min(town[box_list[i][0]-1:box_list[i][1]])
+        for x in range(box_list[i][0] - 1, box_list[i][1] - 1):
+            town[x] -= deli
+    else :
+        deli = 0
     result += deli
 
 print(result)
