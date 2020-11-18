ff66d81ceafe842c32496e85e5481554cb50b07e
Minji KANG <35449625+MinjiKang95@users.noreply.github.com>
2020-11-18 14:08:00 +0900

    Create minji.py

---
+++ DAY71-BOJ6757-팰린드롬진법/minji.py
@@ -0,0 +1,31 @@
+import sys
+
+def sol():
+    s = set([])
+    m = 0
+    for i in range(2, int(x**0.5)+2):
+        d, n = [], x
+        while n > 0:
+            d.append(n % i)
+            n //= i
+
+        k = len(d)
+        for j in range(k//2):
+            if d[j] != d[k-j-1]:
+                break
+            if j == k//2-1:
+                s.add(i)
+        m = i
+    for i in range(m, 0, -1):
+        if x % i == 0:
+            b = x // i-1
+            if b*b-1 > x:
+                s.add(b)
+
+    for e in sorted(s):
+        print(e)
+
+if __name__ == "__main__":
+    x = int(sys.stdin.readline())
+
+    sol()
