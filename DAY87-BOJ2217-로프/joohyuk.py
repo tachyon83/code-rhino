169946fbc4292c5c866dbf45addbad3161031d92
tachyon83 <49609847+tachyon83@users.noreply.github.com>
2020-12-02 13:47:03 +0900

    Create joohyuk.py

---
+++ DAY87-BOJ2217-로프/joohyuk.py
@@ -0,0 +1,10 @@
+import sys
+si = sys.stdin.readline
+
+n, r, mx = int(si()), [], 0
+for _ in range(n):
+    r.append(int(si()))
+r.sort()
+for i, e in enumerate(r):
+    mx = max(mx, (n-i)*e)
+print(mx)
