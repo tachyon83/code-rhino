1c13776e46682684153a529751700983bab52d13
Minji KANG <35449625+MinjiKang95@users.noreply.github.com>
2020-12-08 13:43:32 +0900

    Create minji.py

---
+++ DAY92-BOJ1197-최소스패닝트리/minji.py
@@ -0,0 +1,36 @@
+import sys
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+if __name__ == "__main__":
+    v, e = list(map(int, sys.stdin.readline().split()))
+    parent = [0] * (v+1)
+    edges = []
+    res = 0
+
+    for i in range(1, v+1):
+        parent[i] = i
+
+    for _ in range(e):
+        a, b, c = list(map(int, sys.stdin.readline().split()))
+        edges.append((c, a, b))
+
+    edges.sort()
+
+    for edge in edges:
+        cost, a, b = edge
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            res += cost
+    print(res)
