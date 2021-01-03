2d7e1042b8c96617c02d0f876c7ec06985d45a6a
Minji KANG <35449625+MinjiKang95@users.noreply.github.com>
2021-01-03 21:25:00 +0900

    Create minji.py

---
+++ DAY115-LEET948-BagOfTokens/minji.py
@@ -0,0 +1,16 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], P: int) -> int:
+        tokens.sort()
+        l, r, score = 0 , len(tokens) - 1, 0
+        while l <= r:
+            if P >= tokens[l]:
+                P -= tokens[l]
+                score += 1
+                l += 1
+            elif score and l != r:
+                P += tokens[r]
+                score -= 1
+                r -= 1
+            else:
+                break
+        return score
