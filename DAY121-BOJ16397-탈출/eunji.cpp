ff07efa9890c416cc7b346e0670b427211085574
Eunji <45475492+dmswl6310@users.noreply.github.com>
2021-01-11 12:04:51 +0900

    Create eunji.cpp

---
+++ DAY121-BOJ16397-탈출/eunji.cpp
@@ -0,0 +1,53 @@
+//반례를 못찾겠습니다..
+#include<iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+void BFS(int N, int T, int G);
+
+int main() {
+	int N, T, G;
+	cin >> N >> T >> G;
+	BFS(N, T, G);
+}
+
+void BFS(int N, int T, int G) {
+	queue <pair<int, int>> q;
+	int turn = 0;
+	q.push({ N,0 });
+	bool visit[100000] = { false, };
+	visit[N] = true;
+
+	while (!q.empty()) {
+		int num = q.front().first;
+		int turn = q.front().second;
+
+		if (num == G) {
+			cout << turn;
+			return;
+		}
+		if (turn > T) {
+			cout << "ANG";
+			return;
+		}
+		q.pop();
+		//버튼A
+		if (num < 99999 && visit[num + 1] == false) {
+			q.push({ num + 1,turn + 1 });
+			visit[num + 1];
+		}
+		//버튼B
+		num *= 2;
+		if (num > 99999 || num == 0) continue;
+		string s = to_string(num);
+		s[0]--;
+		num = stoi(s);
+		if (visit[num] == false) {
+			q.push({ num,turn + 1 });
+			visit[num] = true;
+		}
+	}
+	cout << "ANG";
+	return;
+}
