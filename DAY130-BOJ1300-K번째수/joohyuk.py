9581a7696a2b1e8a292fe274a1a1b376281db08c
tachyon83 <49609847+tachyon83@users.noreply.github.com>
2021-01-18 21:14:07 +0900

    Create joohyuk.py

---
+++ DAY130-BOJ1300-K번째수/joohyuk.py
@@ -0,0 +1,15 @@
+N=int(input())
+k=int(input())
+s=1
+e=N*N
+
+while s<e:
+    cnt=0
+    mid=(s+e)//2
+    for i in range(1,N+1):
+        cnt+=min(mid//i,N)
+    
+    if cnt>=k: e=mid
+    else: s=mid+1
+
+print(s)
