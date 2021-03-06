72417891cda2b3939fc066cfd853913c2f38f5af
youngchangyoo <ddori5338@hanmail.net>
2021-03-06 20:21:36 +0900

    Solved 210306

---
+++ DAY167-BOJ1926-그림/youngchang.py
@@ -0,0 +1,37 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+
+board = []
+for _ in range(M):
+    board.append(input().split())
+
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+is_in = lambda r, c: 0 <= r < M and 0 <= c < N
+
+def dfs(r, c):
+    if board[r][c] == '0': return 0
+    if (r, c) in seen: return 0
+    seen.add((r, c))
+    res = 1
+    for d in range(4):
+        nr = r + dr[d]
+        nc = c + dc[d]
+        if is_in(nr, nc):
+            res += dfs(nr, nc)
+    return res
+
+cnt = 0
+big = 0
+seen = set()
+for i in range(M):
+    for j in range(N):
+        if (i, j) not in seen and board[i][j] == '1':
+            big = max(big, dfs(i, j))
+            cnt += 1
+                
+print(cnt)
+print(big)
