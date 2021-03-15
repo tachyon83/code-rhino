e42075c6b82c45dac2a6b87aa57a9cb1e2e180b0
HWNAGJIWON <41350477+HWNAGJIWON@users.noreply.github.com>
2021-03-15 23:57:32 +0900

    Create jiwon.java

---
+++ DAY178-BOJ1753-최단경로/jiwon.java
@@ -0,0 +1,24 @@
+//C로 다시 풀기
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	static int map[][] ;
+	static boolean visit[];
+	
+
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		//sb = new StringBuilder();
+
+		map = new int [5][5];
+		visit = new boolean[100000];
+		
+		
+		
+
+	}
+
+}
