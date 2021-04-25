20e8a65ecea732c3624a6598a1bac743da8251d0
immabe <38066835+immabe@users.noreply.github.com>
2021-04-26 00:00:57 +0900

    Create kimyoungbin.py

---
+++ DAY218-BOJ9372-상근이의 여행/kimyoungbin.py
@@ -0,0 +1,7 @@
+import sys
+t = int(sys.stdin.readline())
+for i in range(t):
+    n, m = map(int, sys.stdin.readline().split())
+    for j in range(m):
+        a, b = map(int, sys.stdin.readline().split())
+    print(n - 1)
