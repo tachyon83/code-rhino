34bea453600ec2594e6e57ae25bf593580cf8c7e
Brian Lee <urleee@naver.com>
2021-05-10 00:10:58 +0900

    Update uiryeong_fail.py

--- DAY231-BOJ15669-나무 위의 입자/uiryeong_fail.py
+++ DAY231-BOJ15669-나무 위의 입자/uiryeong_fail.py
@@ -6,9 +6,9 @@ input = stdin.readline
 N, M = map(int, input().split())
 
 for _ in range(N):
-    u, v = map(int, sys.stdin.readline().split())
+    u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
 for _ in range(M):
-    U, V, C = map(int, sys.stdin.readline().split())
+    U, V, C = map(int, input().split())
