3a60c5516ab33d68e2e47bfb8df3245bdedd9261
guno-kim <64324992+guno-kim@users.noreply.github.com>
2021-05-31 23:52:48 +0900

    Update guno.py

--- DAY250-BOJ1562-계단수/guno.py
+++ DAY250-BOJ1562-계단수/guno.py
@@ -19,3 +19,23 @@ for _ in range(1,n):
     dp=dp_next
 
 print(sum([dp[i][1023] for i in range(10)])%1000000000)
+
+#-----------------다른 풀이-------------------------------------
+import copy
+N=int(input())
+dp = [[0, 0, 0, 0]] + [[1, 0, 0, 0] for i in range(8)] + [[0, 0, 1, 0]]
+next_dp = [[0, 0, 0, 0] for i in range(10)]
+# 한칸씩 움직이므로 0,9 만 방문했으면 모두 방문한것이다
+# 0열: 0,9 모두 미방문, 1열: 0방문, 2열: 9방문, 3열: 0,9모두 방문
+
+for n in range(1, N):
+    for i in range(1, 9):
+        for f in range(4):
+            next_dp[i][f] = dp[i - 1][f] + dp[i + 1][f]
+    for f in [0, 2]:
+        next_dp[0][f | 1] = dp[1][f] + dp[1][f | 1]
+    for f in [0, 1]:
+        next_dp[9][f | 2] = dp[8][f] + dp[8][f | 2]
+    dp=copy.deepcopy(next_dp)
+
+print(sum([e[3] for e in dp])%10**9)
