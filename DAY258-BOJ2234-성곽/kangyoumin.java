47e7e9ff46d38a40856d255d5fe33feb914088f0
Youmin Kang <48348800+kangum99@users.noreply.github.com>
2021-06-07 02:17:39 +0900

    Update kangyoumin.java

--- DAY258-BOJ2234-성곽/kangyoumin.java
+++ DAY258-BOJ2234-성곽/kangyoumin.java
@@ -1,7 +1,9 @@
 
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -10,27 +12,22 @@ import java.util.StringTokenizer;
 class Room {
 	int x;
 	int y;
-//	방 사이즈
-	int size;
-//	벽을 제거할지 말지
-	boolean remove;
 	
-	public Room (int x, int y, int size, boolean remove) {
+	public Room (int x, int y) {
 		this.x = x;
 		this.y = y;
-		this.size = size;
-		this.remove = remove;
+
 	}
 }
 
 public class Main {
-	static int[] dx = {1, 0, -1, 0};
-	static int[] dy = {0, 1, 0, -1};
-	static int n, m;
-
+//	서북동남
+	static int[] dx = {0, -1, 0, 1};
+	static int[] dy = {-1, 0, 1, 0};
+	static int n, m, max = 0, roomNum = 0, size = 1;
+	static HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();	
 	static boolean[][] visit;
 	static int[][] arr;
-	static ArrayList<Integer> list = new ArrayList<Integer>();
 
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
@@ -49,44 +46,70 @@ public class Main {
 		
 		for(int i = 0; i < m; i++) {
 			for(int j = 0; j < n; j++) {
-//				둘다 true가 아닐때!
 				if(!visit[i][j]) {
-					bfs(i, j);
+					roomNum++;
+					bfs1(i, j, roomNum);
+					map.put(roomNum, size);
 				}	
 			}
 		}
+//		방의 개수
+		System.out.println(roomNum);
+//		방의 최대넓이
+		System.out.println(max);
+		max = 0;
+		
+//		벽을 뚫을 때 
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				visit = new boolean[i][j];
+				bfs2(i, j);
+			}
+		}
+		System.out.println(max);
 		
 	}
 	
-	static void bfs (int x, int y) {
+	static void bfs1 (int x, int y, int roomNum) {
 		Queue<Room> q = new LinkedList<Room>();
-		q.add(new Room(x, y, 1, false));
+		q.add(new Room(x, y));
 		visit[x][y] = true;
+		size = 1;
 
 		while(!q.isEmpty()) {
 			Room room = q.poll();
+			int val = arr[room.x][room.y];
+//			룸 넘버로 바꿔주기
+			arr[room.x][room.y] = roomNum;
+//			서북동남에 벽이 있는지 없는지 0001, 0010, 0100, 1000
+			int[] wall = {val & (1<<0), val & (1<<1), val & (1<<2), val & (1<<3)};
 			for(int i = 0; i < 4; i++) {
 				int _x = room.x + dx[i];
 				int _y = room.y + dy[i];
-				if(visit[_x][_y] || _x < 0 || _y > 0 || _x >= n || _y >= m) continue;
-//				벽이 없을 때
-				if(!isWall(room.x, room.y, dx[i], dy[i])) {
-					q.add(new Room(x, y, ++room.size, false));
-					visit[x][y] = true;
-				}
-//				벽이 있을 때				
-				else {
-					
-				}
-
+				if(_x < 0 || _y < 0 || _x >= m || _y >= n) continue;
+//				벽이 있다
+				if(visit[_x][_y] || wall[i] != 0) continue; 
 				
+				q.add(new Room(_x, _y));
+				visit[_x][_y] = true;
+				size++;
 				
 			}
 		}
-		
+		max = Math.max(max, size);
 	}
-	static boolean isWall (int x, int y, int Dx, int Dy) {
-		return true;
+	
+	static void bfs2(int x, int y) {
+		int thisRoom = arr[x][y];
+		for(int i = 0; i < 4; i++) {
+			int _x = x + dx[i];
+			int _y = y + dy[i];
+			if(_x < 0 || _y < 0 || _x >= m || _y >= n) continue;
+			
+			int thatRoom = arr[_x][_y];
+			if(thisRoom == thatRoom) continue;
+			else max = Math.max(max, map.get(thisRoom) + map.get(thatRoom));
+		}
 	}
-
+	
 }
