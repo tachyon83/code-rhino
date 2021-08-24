f5d85277e743ba9952ca61863f175734aade0aca
Youmin Kang <48348800+kangum99@users.noreply.github.com>
2021-08-24 12:52:12 +0900

    Update kangyoumin.java

--- DAY324-BOJ1699-제곱수의 합/kangyoumin.java
+++ DAY324-BOJ1699-제곱수의 합/kangyoumin.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 동적 프로그래밍 
-// 제곱수의 합
 public class Main {
 	static int[] dp;
 	
@@ -11,15 +9,11 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.valueOf(br.readLine());
 		dp = new int[100001];
-	
-		// 대신, 1이 몇 개 존재하는지 초기 설정을 해준다. (최댓값으로 초기 설정)
 		for(int i = 1; i <= n; i++) {
 			dp[i] = i;
 		}
 		
 		for(int i = 1; i <= n; i++) {
-			
-			// 최소가 되는 제곱수 = n의 이전 수 들 중 제곱수 + n - 제곱수의 최수항 개수 
 			for(int j = 1; j * j <= i; j++) {
 				if(dp[i] > dp[i - (j * j)] + 1) { // 
 					dp[i] = dp[i - (j * j)] + 1;
