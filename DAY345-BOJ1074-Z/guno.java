961fcb3f9785fd770c1d7ad12e36b7618b18e8ec
guno-kim <64324992+guno-kim@users.noreply.github.com>
2021-09-25 03:26:28 +0900

    Update guno.java

--- DAY345-BOJ1074-Z/guno.java
+++ DAY345-BOJ1074-Z/guno.java
@@ -6,59 +6,36 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder sb=new StringBuilder();
     static StringTokenizer st;
+    static int getInt(){
+        return Integer.parseInt(st.nextToken());
+    }
 
-    static int n;
-    static boolean[][] isWall;
-    static int[][] dp1;
-    static int[][] dp2;
-    static int[][] dp3;
     public static void main(String[] args) throws Exception {
 
-        n = Integer.parseInt(br.readLine());
-        isWall = new boolean[n][n];
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                int num = Integer.parseInt(st.nextToken());
-                if(num==1)
-                    isWall[i][j] = true;
+        st = new StringTokenizer(br.readLine());
+        int n = getInt(), r = getInt(), c = getInt();
+
+        int len = (int) Math.pow(2, n);
+        int ans = 0;
+        while(len>=2){
+            len/=2;
+            int area = len * len;
+            int cnt = 0;
+
+            if(r>=len){
+                r-=len;
+                cnt+=2;
             }
-        }
-
-        dp1 = new int[n][n];
-        dp2 = new int[n][n];
-        dp3 = new int[n][n];
-        dp1[0][1] = 1;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                visit(i,j);
+            if(c>=len){
+                c-=len;
+                cnt+=1;
             }
+            ans += cnt * area;
         }
-
-        sb.append(dp1[n-1][n-1]+dp2[n-1][n-1]+dp3[n-1][n-1]);
+        sb.append(ans);
 
         bw.write(sb.toString());bw.flush();bw.close();br.close();
     }
 
-    static void visit(int x,int y){
-        int right = dp1[x][y], down = dp2[x][y], diag = dp3[x][y];
-        moveRight(x,y,right+diag);
-        moveDown(x,y,down+diag);
-        moveDiag(x,y,right+down+diag);
-    }
-
-    static void moveRight(int x,int y,int num){
-        if(y<n-1 && !isWall[x][y+1])
-            dp1[x][y+1]+=num;
-    }
-    static void moveDown(int x,int y,int num){
-        if(x<n-1 && !isWall[x+1][y])
-            dp2[x+1][y]+=num;
-    }
-    static void moveDiag(int x,int y,int num){
-        if(x<n-1 && y<n-1 && !isWall[x][y+1] && !isWall[x+1][y] && !isWall[x+1][y+1])
-            dp3[x+1][y+1]+=num;
-    }
 
 }
