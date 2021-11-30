3a7c4894ce8324948c90c854530afa0ac30f6703
asdf99245 <39851220+asdf99245@users.noreply.github.com>
2021-11-30 15:43:07 +0900

    Update yunkyu.js

--- DAY385-PROGRAMMERS-호텔방배정/yunkyu.js
+++ DAY385-PROGRAMMERS-호텔방배정/yunkyu.js
@@ -1,26 +1,21 @@
-// 효율성 1번 제외 틀림
 const map = new Map();
 
-function solution(k, room_number) {
-    var answer = [];
-    for(let i = 1; i <= k; i++){
-        map[i] = true;
+const find = (n) => {
+    if(!map.has(n)){
+        map.set(n,n+1);
+        return n;
     }
-    
+    const par = find(map.get(n));
+    map.set(n,par+1);
+    return par;
+}
+
+function solution(k, room_number) {
+    const answer = [];
     for(let i = 0; i < room_number.length; i++){
-        if(map[room_number[i]]){
-            map[room_number[i]] = false;
-            answer.push(room_number[i]);
-        }
-        else {
-            for(let j = room_number[i]; j <= k; j++) {
-                if(map[j]) {
-                    answer.push(j);
-                    map[j] = false;
-                    break;
-                }
-            }
-        }
+        const cur = room_number[i];
+        const next = find(cur);
+        answer.push(next);
     }
     return answer;
 }
