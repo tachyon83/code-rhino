2843b87405b23c9699e83e1e0b80bf201d148571
Youmin Kang <48348800+kangum99@users.noreply.github.com>
2021-12-14 21:36:45 +0900

    Create kangyoumin.java

---
+++ DAY395-PROGRAMMERS-프린터/kangyoumin.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        int l = location;
+
+        Queue<Integer> que = new LinkedList<Integer>();
+        for(int i : priorities){
+            que.add(i);
+        }
+
+        Arrays.sort(priorities);
+        int size = priorities.length-1;
+
+
+
+        while(!que.isEmpty()){
+            Integer i = que.poll();
+            if(i == priorities[size - answer]){
+                answer++;
+                l--;
+                if(l <0)
+                    break;
+            }else{
+                que.add(i);
+                l--;
+                if(l<0)
+                    l=que.size()-1;
+            }
+        }
+
+        return answer;
+    }
+}
