b1f89fc82128d93b9b027dab2094bb197ca126cb
NK <96505696+NK590@users.noreply.github.com>
2022-03-07 07:12:27 +0900

    Create hyunwoo.py

---
+++ DAY442-PROGRAMMERS-멀쩡한사각형/hyunwoo.py
@@ -0,0 +1,14 @@
+# 최소공배수 함수
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def solution(w, h):
+    # 주어진 가로 격자와 세로 격자의 최소공배수
+    div = gcd(w, h)
+    # 전체 타일 수
+    total_tile = w * h
+    # 빠지는 타일 수 = 대각선이 지나가는 최소단위 격자 * (대각선이 닿는 타일 수)
+    remove_tile = div * (w//div + h//div - 1)
+    return total_tile - remove_tile
