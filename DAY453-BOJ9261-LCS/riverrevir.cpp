2768a026fdfd01ea0392855417fbb23141cfb5f8
DG LEE <81510864+riverrevir@users.noreply.github.com>
2022-03-24 08:11:19 +0900

    Create riverrevir.cpp

---
+++ DAY453-BOJ9261-LCS/riverrevir.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <math.h>
+#include <stack>
+#include <limits.h>
+using namespace std;
+int dp[1001][1001];
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    string a, b;
+    cin >> a >> b;
+    for (int i = 1; i <= a.size(); i++) {
+        for (int j = 1; j <= b.size(); j++) {
+            if (a[i-1] == b[j-1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    cout << dp[a.size()][b.size()];
+}
