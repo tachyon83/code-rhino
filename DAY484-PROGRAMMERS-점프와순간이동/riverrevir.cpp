8354948417d035755566e613860c849fae49ccab
DG LEE <81510864+riverrevir@users.noreply.github.com>
2022-05-16 01:14:00 +0900

    Create riverrevir.cpp

---
+++ DAY484-PROGRAMMERS-점프와순간이동/riverrevir.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int solution(int n)
+{
+    int ans = 0;
+    while(n > 0)
+    {
+        if(n % 2 == 0) n /= 2;
+        else
+        {
+            n -= 1;
+            ans++;
+        }
+    }
+    return ans;
+}
+
