333b4760e7734463678ed4867295a5c71848786c
DG LEE <81510864+riverrevir@users.noreply.github.com>
2022-06-21 01:06:23 +0900

    Create riverrevir.cpp

---
+++ DAY509-PROGRAMMERS_멀쩡한 사각형/riverrevir.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+int GCD(int a,int b){
+    if(b==0) return a;
+    return GCD(b,a%b);
+}
+long long solution(int w,int h) {
+    long long answer=1;
+    answer=((long long)w*h)-(w+h)+GCD(w,h);
+    return answer;
+}
